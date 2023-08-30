c61522dd226a9c9be2683ade5e62da4385e5dda0
gines18 <ruczkowski.m@gmail.com>
2023-08-30 16:17:01 +0100

    change background for Welcome header

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -944,7 +944,7 @@ export const job = [
 
 export const component = [
   {
-    title: <span> <span className="redColor">Crafting Engaging User Experiences  <br /> Through Frontend Web Development</span></span>,
+    title: <span> <span className="gradient-text">Crafting Engaging User Experiences  <br /> Through Frontend Web Development</span></span>,
     imgSrc: "./ilu3.jpeg",
  
   },
