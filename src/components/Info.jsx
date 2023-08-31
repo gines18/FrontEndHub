2faf190a38005d9d758ec28620f0fc2c9c1ecf95
gines18 <ruczkowski.m@gmail.com>
2023-08-31 10:30:55 +0100

    change id names in Info component

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -5,34 +5,32 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 function Info({ component }) {
-
-
-useLayoutEffect(() => {
+  useLayoutEffect(() => {
     const animation = document.getElementById("info-component");
     let ctx = gsap.context(() => {
-    gsap.fromTo(
-      animation.children,
-      { y: "+=100", opacity: 0 },
-      {
-        y: 0,
-        opacity: 1,
-        duration: 2.5,
-        scrollTrigger: {
-          trigger: animation,
-          start: "top 70%",
-        },
-      }
-    );
-  });
+      gsap.fromTo(
+        animation.children,
+        { y: "+=100", opacity: 0 },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 2.5,
+          scrollTrigger: {
+            trigger: animation,
+            start: "top 70%",
+          },
+        }
+      );
+    });
     return () => ctx.revert();
   }, []);
 
   return (
     <div id="info-component">
       {component.map((item, index) => (
-        <div id="info-component-data" key={index}>
-          <p id="info-text">{item.title}</p>
-          <img id="info-img" src={item.imgSrc} alt="My Image" />
+        <div key={index}>
+          <p id="nice-text">{item.title}</p>
+          <img id="cool-img" src={item.imgSrc} alt="My Image" />
         </div>
       ))}
     </div>
