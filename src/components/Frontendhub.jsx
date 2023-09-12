94a15dd5549bdd3b9abc8c5634e856696a85a922
gines18 <ruczkowski.m@gmail.com>
2023-09-12 11:01:27 +0100

    add gsap animation for Frontendhub component

--- src/components/Frontendhub.jsx
+++ src/components/Frontendhub.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useLayoutEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function Frontendhub() {
+
+  useLayoutEffect(() => {
+    const animation = document.getElementById("frontend");
+    let ctx = gsap.context(() => {
+      gsap.fromTo(
+        animation.children,
+        { y: "+=50", opacity: 0 },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 2.5,
+          scrollTrigger: {
+            trigger: animation,
+            start: "top 70%",
+            // markers: true,
+          },
+        }
+      );
+    });
+    return () => ctx.revert();
+  }, []);
+
   return (
-    <div className='frontend'>
+    <div id='frontend'>
         <p className='gradient-text'>Frontendhub</p>
         </div>
   )
