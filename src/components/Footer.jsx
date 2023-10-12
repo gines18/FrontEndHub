08ccd2e6c7bd421aae029c4fe4824e2586ed7fbb
gines18 <ruczkowski.m@gmail.com>
2023-10-12 23:30:18 +0100

    add date in Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./Styles.css";
+import moment from "moment";
 
 function Footer() {
   return (
 <div id="container-footer">
+ <div>Up to date:  <span className="date">{moment().format("MMMM Do, YYYY")}</span></div>
  <a href="https://github.com/gines18/" target="_blank">Github</a>
 </div>
   );
