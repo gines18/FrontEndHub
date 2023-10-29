cb17ed8675c5c0fa658b9d3244c91c3dd7e095b8
gines18 <ruczkowski.m@gmail.com>
2023-10-29 13:20:22 +0000

    show message if News articles are not displayed.

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -3,6 +3,7 @@ import * as React from "react";
 
 function News() {
   const [articles, setArticles] = useState([]);
+  const [showArticles, setShowArticles] = useState(false); // Add a state to control whether to display articles or not
 
   useEffect(() => {
     const apikey = "2e7a023c0e31506f9945a4fc2eedac41";
@@ -16,29 +17,36 @@ function News() {
       })
       .then(function (data) {
         setArticles(data.articles);
+        setShowArticles(true); // Set the state to true when articles are fetched
       });
   }, []);
 
   return (
     <>
-    <div className="header-news">
-      <p>Take a break and read <span className="underline-news">Latest News</span></p>
-    </div>
+      <div className="header-news">
+        <p>
+          Take a break and read <span className="underline-news">Latest News</span>
+        </p>
+      </div>
       <div className="container-news">
-        {articles.map((article, index) => (
-          <div className="card-news" key={index}>
-            <img
-              src={article.image}
-              style={{ width: "100%", height: "150px" }}
-              alt=""
-            />
-            <h3 className="news-title">{article.title}</h3>
-            <p>{article.description}</p>
-            <a className="news-link" href={article.url} target="_blank">
-              Read more
-            </a>
-          </div>
-        ))}
+        {setShowArticles === true ? (
+          articles.map((article, index) => (
+            <div className="card-news" key={index}>
+              <img
+                src={article.image}
+                style={{ width: "100%", height: "150px" }}
+                alt=""
+              />
+              <h3 className="news-title">{article.title}</h3>
+              <p>{article.description}</p>
+              <a className="news-link" href={article.url} target="_blank">
+                Read more
+              </a>
+            </div>
+          ))
+        ) : (
+          <p id="no-articles">No articles to display. Check later.</p>
+        )}
       </div>
     </>
   );
